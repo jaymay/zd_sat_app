3300164a8c852e66d97d011db6eaf711adeb85a9
Jason Smale <jsmale@zendesk.com>
2013-08-26 23:50:33 +1000

    Switch to new type of app layout

--- app.js
+++ app.js
@@ -2,35 +2,23 @@
   return {
 
     events: {
-      'app.activated':'doSomething',
-      'click .predict':'sendData'
+      'app.activated':'init'
     },
 
-    requests: {
-      'postData' : function(data){
-        return {
-          // For more info about jQuery AJAX -> http://api.jquery.com/jQuery.ajax/
-          url: 'https://www.googleapis.com/prediction/v1.5/hostedmodels/zendesk/predict',
-          type: 'POST',
-          data: data
-        };
-      }
-    },
+    init: function() {
+      // Hide 2 custom fields
+      var ratingField = this.ticketFields('custom_field_' + this.setting('predictionRating'));
+      ratingField.hide();
 
-    doSomething: function() {
+      var scoreField = this.ticketFields('custom_field_' + this.setting('predictionScore'));
+      scoreField.hide();
+
+      // Render layout
       this.switchTo('main', {
-        id: this.ticket().id(),
-        subject: this.ticket().subject(),
-        description: this.ticket().description()
-      });
-    },
-
-    // When the button with .predict is pressed, this function will fire
-    sendData: function() {
-      // Which then calls the request "postData", and should pass through the data (at the moment it's an empty object)
-      this.ajax( 'postData', {
-        id: this.ticket().id, description: this.ticket().description()
+        rating: ratingField.value(),
+        score: scoreField.value()
       });
     }
+
   };
 }());
